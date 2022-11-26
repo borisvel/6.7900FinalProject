0c997d6dca9f671ea2278795bc46171d998ac289
Boris Velasevic <borisvel@mit.edu>
2022-11-26 01:46:40 -0500

    Changed PIL to cv2, architecture notebook

--- image_to_csv.py
+++ image_to_csv.py
@@ -2,37 +2,41 @@ import os
 import pandas as pd
 import numpy as np
 from PIL import Image
+import cv2
+import cvzone
+from cvzone.SelfiSegmentationModule import SelfiSegmentation
+
+segmentor = SelfiSegmentation()
 
 
+#directory = ["bayc", "unicorns"]
+directory = ["bayc"]
 
-directory = ["unicorns", "bayc"]
-
-
-def flatten(l):
-    res = []
-    for r, g, b in l:
-        res.extend([r, g, b])
-    return res
-
+def remove_background(img):
+    background_color = img[4][4]
+    size = img.shape[0]
+    img_copy = np.copy(img)
+    for i in range(size):
+        for j in range(size):
+            if np.linalg.norm(img_copy[i][j] - background_color, ord=2) <= 0.001:
+                img_copy[i][j] = np.array([255, 255, 255])
+    return img_copy
 
 
 for directory in directory:
     images = []
+    c = 0
     for filename in os.listdir(directory):
+        if (c + 1)%1000 == 0: print(c + 1)
         f = os.path.join(directory, filename)
         if f[-3::] not in {"png", "jpg"}: continue
-        img = Image.open(f)
-        img = img.convert("RGB")
-        img = img.resize((200, 200))
-        img_data = img.getdata()
-        temp = list(img_data)
-        n = len(temp)
-        side = int(np.sqrt(n))
-        pixels = [0]*n
-        for i in range(side):
-            for j in range(side):
-                pixels[200*i + j] = temp[200*j + i]
-        images.append(flatten(pixels))
+        img = cv2.imread(f, cv2.IMREAD_COLOR)
+        img = cv2.resize(img, (128, 128), interpolation=cv2.INTER_AREA)
+        #if directory == "bayc": img = remove_background(img)
+        cv2.imwrite(directory + "_resize2/" + directory + "_" + str(c) + ".png", img)
+        flat = img.flatten()
+        images.append(flat)
+        c += 1
     A = np.array(images)
     print(A.shape)
     df = pd.DataFrame(A, columns=[str(i) for i in range(A.shape[1])])
